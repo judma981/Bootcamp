8b87e78e3dbd9c77b09a0c67dfffa8228e1174a2
Juan David <judama981@hotmail.com>
2024-07-31 07:54:44 -0500

    31/07/2024
    Javascript 101

---
+++ unidad_3/hola.js
@@ -0,0 +1,32 @@
+//console.log("hola Mundo");
+//console.log("  " + "aleatorio"); 
+
+
+//vars
+
+let mensaje = "Hola mundo  bootcamp";
+
+let grup = 158;
+const PI =3.14159;
+let resultado = grup * PI;
+
+//console.log(mensaje);
+//console.log(grup + " * "+ PI + " = " + resultado);
+
+
+let Nombre = "Juan";
+let Edad = 26;
+const Cedula_ciudadania = 987654321;
+console.log("Mi nombre es " + Nombre + "  tengo "+ Edad + " años, y mi numero de cedula es " + Cedula_ciudadania);
+
+let usuario = {
+    Nombre: "juan",
+    Edad : 26, 
+};
+console.log(usuario);
+
+
+let frutas = ["manzana","mango","pera"];
+console.log(frutas);
+frutas[3] = "sandia";
+console.log(frutas);
