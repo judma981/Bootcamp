37bea056c0d0ad08776bff0fe2a7cbac098de104
Juan David <judama981@hotmail.com>
2024-08-08 06:18:46 -0500

    06/08/2024

--- unidad_3/hola.js
+++ unidad_3/hola.js
@@ -1,32 +1,21 @@
-//console.log("hola Mundo");
-//console.log("  " + "aleatorio"); 
 
 
-//vars
+// Generar un número aleatorio
+let numeroAleatorio = 10*Math.random();
+let NumeroAleatorio = Math.ceil(numeroAleatorio);
+//var objetivo = document.getElementById("texto_nav1");
+//objetivo.innerHTML = NumeroAleatorio;
+// Ingresar número
+//let NumeroIngresado = 8;
+let NumeroIngresado = prompt("ingresa un número del 1 al 10:")
+console.log("aleatorio: " + NumeroAleatorio);
+console.log("ingresado: " + NumeroIngresado);
 
-let mensaje = "Hola mundo  bootcamp";
-
-let grup = 158;
-const PI =3.14159;
-let resultado = grup * PI;
-
-//console.log(mensaje);
-//console.log(grup + " * "+ PI + " = " + resultado);
-
-
-let Nombre = "Juan";
-let Edad = 26;
-const Cedula_ciudadania = 987654321;
-console.log("Mi nombre es " + Nombre + "  tengo "+ Edad + " años, y mi numero de cedula es " + Cedula_ciudadania);
-
-let usuario = {
-    Nombre: "juan",
-    Edad : 26, 
-};
-console.log(usuario);
-
-
-let frutas = ["manzana","mango","pera"];
-console.log(frutas);
-frutas[3] = "sandia";
-console.log(frutas);
+// Si son iguales imprimir "Has ganado"
+if(NumeroAleatorio === NumeroIngresado){
+    alert("Has ganado");
+    
+}else{
+   
+    alert("Sigue participando :" + NumeroIngresado + " numero a adivinar  "+ NumeroAleatorio);
+}
