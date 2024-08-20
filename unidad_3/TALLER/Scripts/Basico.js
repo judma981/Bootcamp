10653a59ee4213e7a3ddd9ce88e995585148195c
Juan David <judama981@hotmail.com>
2024-08-20 07:57:54 -0500

    20/08/2024
    Taller Js/ Index diseño y maquetación de la pag

---
+++ unidad_3/TALLER/Scripts/Basico.js
@@ -0,0 +1,36 @@
+ /*//1.
+  //  const nombre = prompt ("Ingrese Su Nombre");
+  //  const edad = prompt ("Ingrese Su Edad");
+  //  alert("Hola, " + nombre + " Es un placer conocerte. Tienes " + edad + " años.");
+
+  console.log("----------------------------------------------------------------------")    
+  //2.
+      let Numero = 53;
+      let Texto = "Hola mundo";
+      let booleano = true;
+  
+      let Numero1 = 84;
+      let Sumatoria = Numero + Numero1;
+      let Sustracion = Numero1 - Numero;
+  
+      console.log("La Suma de dos numeros :" + Sumatoria + " La resta de dos numeros :" + Sustracion );
+  console.log("----------------------------------------------------------------------") 
+  
+  //3.
+ 
+  const Altura = prompt ("Ingrese la altura del triangulo");
+  const Lado = prompt ("Ingrese el valor del lado del triangulo");
+  const Base = prompt ("Ingrese el valor de la base del triangulo");
+  const Area = (Base * Altura)/ 2 ;
+  const perímetro = Lado + Base + Lado ;
+  
+  console.log("El area es :" + Area +"El perimetro es " + perímetro);*/
+  
+ // 4.
+    
+    let Numero = 502;
+    if (Numero % 2 ===0){
+        console.log("par")
+    }else{
+        console.log("impar")
+    }
---
+++ unidad_3/TALLER/Scripts/Intermedio.js
@@ -0,0 +1,21 @@
+/*
+
+Nivel Intermedio:
+Bucles:
+7. Imprimir los números del 1 al 100.
+8. Crear una tabla de multiplicar de un número ingresado por el usuario.
+9. Implementar un programa que calcule el factorial de un número.
+
+Funciones:
+10. Crear una función que convierta grados Celsius a Fahrenheit.
+11. Implementar una función que determine si una cadena es un palíndromo.
+
+Uso de Bibliotecas:
+12. Utilizar una biblioteca como Moment.js para realizar operaciones con fechas y horas.
+13. Crear un sencillo juego utilizando una biblioteca de gráficos como Phaser.
+*/
+//12.
+const moment = require('moment')
+const Fechas = moment();
+
+console.log(Fechas.format());
